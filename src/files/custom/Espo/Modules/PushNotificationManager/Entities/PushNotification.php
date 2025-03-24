e988ce4346091bebd4f729a096e55444e8bcdf17
Anton Slyzhko <anton.slyzhko@gmail.com>
2025-03-24 08:32:53 +0200

    new entities

---
+++ src/files/custom/Espo/Modules/PushNotificationManager/Entities/PushNotification.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Espo\Modules\PushNotificationManager\Entities;
+
+use Espo\Core\ORM\Entity;
+
+class PushNotification extends Entity
+{
+    public const ENTITY_TYPE = 'PushNotification';
+}
---
+++ src/files/custom/Espo/Modules/PushNotificationManager/Entities/PushNotificationTemplate.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Espo\Modules\PushNotificationManager\Entities;
+
+use Espo\Core\ORM\Entity;
+
+class PushNotificationTemplate extends Entity
+{
+    public const ENTITY_TYPE = 'PushNotificationTemplate';
+}
