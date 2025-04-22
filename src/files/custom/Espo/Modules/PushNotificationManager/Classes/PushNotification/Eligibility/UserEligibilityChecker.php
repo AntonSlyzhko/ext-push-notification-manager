4002c3496c9523b77ca82a4e0bb2324c8765ed34
Anton Slyzhko <a.slyzhko@astracosm.com>
2025-04-22 11:22:42 +0300

    doc

--- src/files/custom/Espo/Modules/PushNotificationManager/Classes/PushNotification/Eligibility/UserEligibilityChecker.php
+++ src/files/custom/Espo/Modules/PushNotificationManager/Classes/PushNotification/Eligibility/UserEligibilityChecker.php
@@ -6,8 +6,6 @@ use Espo\Entities\User;
 
 /**
  * Determines whether to send push notification to user via a particular channel
- *
- * @noinspection PhpUnused
  */
 interface UserEligibilityChecker
 {
